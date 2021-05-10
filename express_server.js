e47afdbefb87eeafdb0e838ccfcd9279e97a1ab5
Claire Devlin <claire.devlin@hotmail.com>
2021-05-10 15:15:35 -0400

    added URLS page with table

--- express_server.js
+++ express_server.js
@@ -9,16 +9,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (re, res) => {
+app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (re, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (re, res) => {
+app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 
