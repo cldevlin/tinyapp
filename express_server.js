4b2276ccd2ac43d1e93fc4d65b9336da2cccd32b
Claire Devlin <claire.devlin@hotmail.com>
2021-05-10 18:56:35 -0400

    created form submission page and implemented logic to create random string of 6 chars

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
@@ -8,6 +11,14 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let output = "";
+  for (let i = 0; i < 6; i++) {
+    output += String.fromCharCode(Math.floor(Math.random() * 26 + 65 + 32));
+  }
+  // console.log(output)
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -26,10 +37,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 
 
 app.listen(PORT, () => {
